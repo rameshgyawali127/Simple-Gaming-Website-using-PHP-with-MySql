ec1da51a2b59f456d9f61985ce7b79d30f936828
Ramesh Gyawali <125185463+rameshgyawali127@users.noreply.github.com>
2024-05-14 18:20:21 +0545

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "testing");
-if (mysqli_connect_error()) {
-    echo "<script>alert('Cannot connect to the database');</script>";
-    exit();
-}
-?>
